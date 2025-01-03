367fa9da43a114bdc6310c39ea15faff83000f16
Ryan <wulujason085@gmail.com>
2025-01-03 16:05:27 +0800

    加入專案檔案。

---
+++ prjWebsiteB/Partials/dbGroupBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace prjWebsiteB.Models;
+
+public partial class dbGroupBContext : DbContext
+{
+    public dbGroupBContext() { }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            IConfigurationRoot Config = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+            optionsBuilder.UseSqlServer(Config.GetConnectionString("dbGroupB"));
+        }
+    }
+}
+
